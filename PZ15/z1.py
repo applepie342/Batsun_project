2032538b841531094d70679330eb44b21288d91c
applepie342 <minikrolik128@gmail.com>
2024-05-29 16:56:30 +0300

    PZ16

--- PZ15/z1.py
+++ PZ15/z1.py
@@ -4,27 +4,88 @@
 # Отчество, Дата Рождения, Награды (наличие кр. Диплома или медали (да/нет)), Адрес,
 # выбранная Специальность.
 import sqlite3 as sq
+from students import info_students
+
 
 with sq.connect('abiturient.db') as con:
     cur = con.cursor()
-    # cur.execute("DROP TABLE IF EXISTS anketa")
+    cur.execute("DROP TABLE IF EXISTS anketa")
     cur.execute("""CREATE TABLE IF NOT EXISTS anketa (
-    student_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    surname TEXT NOT NULL,
-    name TEXT NOT NULL,
-    otchestvo TEXT NOT NULL,
-    birthday TEXT NOT NULL,
+    reg_num INTEGER PRIMARY KEY,
+    name TEXT,
+    surname TEXT,
+    otchestvo TEXT,
+    birthday TEXT,
     achievments TEXT,
-    adress TEXT NOT NULL,
-    speciality TEXT NOT NULL
+    adress TEXT,
+    speciality TEXT
     )""")
+
+
+with sq.connect('abiturient.db') as con:
+    cur = con.cursor()
+    cur.executemany("INSERT INTO anketa VALUES (?, ?, ?, ?, ?, ?, ?, ?)", info_students)
 
 # with sq.connect('abiturient.db') as con:
 #     cur = con.cursor()
-#     cur.execute("INSERT INTO anketa VALUES ('1', 'Алексей','Иванов', 'Иванович', 'GRS', 'Да', 'Тургеневская', 'ИС')")
+#     cur.execute("SELECT * FROM anketa")
+#     print(cur.fetchall())
 
+
+# Поиск
 with sq.connect('abiturient.db') as con:
     cur = con.cursor()
-    cur.execute("SELECT * FROM anketa")
-    result = cur.fetchall()
-    print(result)
+
+    # cur.execute("""SELECT * from anketa WHERE name LIKE 'Алексей' """)
+    # print(cur.fetchall())
+
+    # cur.execute("""SELECT * from anketa WHERE birthday LIKE '%2005' """)
+    # print(cur.fetchall())
+    #
+    # cur.execute("""SELECT * from anketa WHERE achievments LIKE 'Да' """)
+    # print(cur.fetchall())
+
+
+# Редактирование
+with sq.connect('abiturient.db') as con:
+    cur = con.cursor()
+
+    # cur.execute("""SELECT * from anketa WHERE reg_num = 2 """)
+    # print(cur.fetchall())
+    # cur.execute("""UPDATE anketa SET birthday = '20.01.2005' WHERE birthday LIKE '%2004' """)
+    # cur.execute("""SELECT * from anketa WHERE reg_num = 2 """)
+    # print(cur.fetchall())
+
+    # cur.execute("""SELECT * from anketa WHERE speciality LIKE 'СА' """)
+    # print(cur.fetchall())
+    # cur.execute("""UPDATE anketa SET speciality = 'КМ' WHERE speciality LIKE 'СА' """)
+    # cur.execute("""SELECT * from anketa WHERE speciality LIKE 'КМ' """)
+    # print(cur.fetchall())
+    #
+    # cur.execute("""SELECT * from anketa WHERE name LIKE 'Ксения' """)
+    # print(cur.fetchall())
+    # cur.execute("""UPDATE anketa SET adress = 'Братский, 21' WHERE name LIKE 'Ксения' """)
+    # cur.execute("""SELECT * from anketa WHERE name LIKE 'Ксения' """)
+    # print(cur.fetchall())
+
+# Удаление
+with sq.connect('abiturient.db') as con:
+    cur = con.cursor()
+
+    cur.execute("""SELECT * from anketa WHERE name LIKE 'Мария' and speciality LIKE 'ИКС' """)
+    print(cur.fetchall())
+    cur.execute("""DELETE from anketa WHERE name LIKE 'Мария' AND speciality LIKE 'ИКС' """)
+    cur.execute("""SELECT * from anketa WHERE name LIKE 'Мария' and speciality LIKE 'ИКС' """)
+    print(cur.fetchall())
+
+    # cur.execute("""SELECT * from anketa WHERE reg_num = 5 """)
+    # print(cur.fetchall())
+    # cur.execute("""DELETE from anketa WHERE reg_num = 5 """)
+    # cur.execute("""SELECT * from anketa WHERE reg_num = 5 """)
+    # print(cur.fetchall())
+    #
+    # cur.execute("""SELECT * from anketa WHERE surname LIKE 'Краснова' """)
+    # print(cur.fetchall())
+    # cur.execute("""DELETE from anketa WHERE surname LIKE 'Краснова' """)
+    # cur.execute("""SELECT * from anketa WHERE surname LIKE 'Краснова' """)
+    # print(cur.fetchall())
